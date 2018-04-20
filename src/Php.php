2f64d5e44e26d5ee07918207f9728779110f5e2b
Samuel Vicent <samuelvicent@gmail.com>
2018-04-20 16:26:04 +0200

    First Commit

---
+++ src/Php.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Atico/SpreadsheetTranslator package.
+ *
+ * (c) Samuel Vicent <samuelvicent@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Atico\SpreadsheetTranslator\Exporter\Php;
+
+use Atico\SpreadsheetTranslator\Core\Exporter\ExportContentInterface;
+use Atico\SpreadsheetTranslator\Core\Exporter\ExporterInterface;
+use Atico\SpreadsheetTranslator\Core\Exporter\AbstractExporter;
+
+class Php extends AbstractExporter implements ExporterInterface
+{
+    function __construct($configuration)
+    {
+        $this->configuration = new PhpExporterConfigurationManager($configuration);
+    }
+
+    public function getFormat()
+    {
+        return 'php';
+    }
+
+    protected function buildContent(ExportContentInterface $exportContent)
+    {
+        return sprintf("<?php\nreturn %s;", var_export($exportContent->getTranslations(), true));
+    }
+
+}
